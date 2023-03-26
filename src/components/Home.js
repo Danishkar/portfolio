15cf2dc0e45a9e82682de9f6e88360b130167e6f
Danishkar <danishkar.s@gmail.com>
2023-03-26 23:19:44 +0530

    change #1

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,10 @@ const Home = () => {
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-pink-600 pl-[5px]'>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+        <h1 className='text-3xl sm:text-7xl font-bold text-[#ccd6f6]'>
           DANISHKAR SIVALINGAM
         </h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+        <h2 className='text-3xl sm:text-7xl font-bold text-[#8892b0]'>
           I'm a Full Stack Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 pl-9 lg:pl-[280px] bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 pl-9 bg-[#0a192f] text-gray-300'>
       <div>
         {/* <img src={Logo} alt='Logo Image' style={{ width: '200px' }} /> */}
         <h2 className='text-[20px] font-bold text-pink-600'>Danishkar S.</h2>
