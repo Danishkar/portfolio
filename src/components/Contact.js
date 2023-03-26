8c89e175374c5f12c42282833c40d4fb420ffd86
Danishkar <danishkar.s@gmail.com>
2023-03-26 23:08:00 +0530

    changes to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,7 +24,7 @@ const Contact = () => {
   };
 
   return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 md:pt-24'>
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 md:pt-64'>
         <form ref={form} onSubmit={sendEmail} className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
